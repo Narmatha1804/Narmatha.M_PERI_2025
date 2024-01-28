37ae4e2d4ada82b1043f10ae6021eff1f0b6ddb8
Narmatha1804 <147385293+Narmatha1804@users.noreply.github.com>
2024-01-28 22:30:23 +0530

    Update Que1.py

--- 25.01.2024/Que1.py
+++ 25.01.2024/Que1.py
@@ -1,9 +1,41 @@
-def handshakes(n):
-    return n * (n - 1) // 2
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
-test_cases = int(input())
-for _ in range(test_cases):
-    attendees = int(input())
-    result = handshakes(attendees)
-    print(result)
+#
+# Complete the 'findPoint' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER px
+#  2. INTEGER py
+#  3. INTEGER qx
+#  4. INTEGER qy
+#
+
+def findPoint(px, py, qx, qy):
+    return str(2*qx-px),str(2*qy-py)
+
+if _name_ == '_main_':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    for n_itr in range(n):
+        first_multiple_input = input().rstrip().split()
+
+        px = int(first_multiple_input[0])
+
+        py = int(first_multiple_input[1])
+
+        qx = int(first_multiple_input[2])
+
+        qy = int(first_multiple_input[3])
+
+        result = findPoint(px, py, qx, qy)
+
+     …
